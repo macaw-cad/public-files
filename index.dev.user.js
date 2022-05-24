2d4798970cdee6aa51a17e3151b919edd8300a4b
Serge van den Oever <serge@macaw.nl>
2022-05-24 15:02:50 +0200

    Add files via upload

---
+++ index.dev.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name        competenceframework-devops-wiki-validate-tampermonkey
+// @namespace   https://macaw.nl/
+// @version     1.0.0
+// @author      Serge van den Oever <serge.van.den.oever@macaw.nl>
+// @source      https://github.com/Trim21/webpack-userscript-template
+// @match       https://dev.azure.com/*
+// @require     file://C:\P\competenceframework\packages\devops-wiki-validate-tampermonkey\dist\index.debug.user.js
+// @grant       GM.xmlHttpRequest
+// @run-at      document-end
+// ==/UserScript==
+
+
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./config/empty.cjs":
+/*!**************************!*\
+  !*** ./config/empty.cjs ***!
+  \**************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./config/empty.cjs"]();
+/******/ 	
+/******/ })()
+;
